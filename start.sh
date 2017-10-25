7ffa71704ee15b58b1d88b25821a98095d5415dc
Matt Small <mbsmall@google.com>
2017-10-25 09:32:34 -0700

    much simpler start.sh Just start a single server, print out some handy URLs for it

--- start.sh
+++ start.sh
@@ -7,28 +7,9 @@
 # subject to an additional IP rights grant found at
 # http://polymer.github.io/PATENTS.txt
 
-start_server() {
-	SERVER=$1
-	PORT=$2
-
-	cat /dev/null > logs/${SERVER}.log
-
-	cd $SERVER
-	echo "serve ${SERVER} on port ${PORT} from `pwd`"
-	../server.py -p ${PORT} &> ../logs/${SERVER}-${PORT}.log &
-	cd ..
-}
-
-# kill the subprocesses when exiting this process
-# to double-check: ps -o pid,ppid,pgid,cmd
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-
-
-start_server "arcs-cdn" 5001
-
-start_server "arc-stories" 8080
-
-start_server "arcs-particles" 8888
-start_server "arcs-custom-events" 9999
-
-parallel --tagstring "{/.}:" --line-buffer tail -f \{\} ::: logs/*.log
+PORT=9000
+echo "starting arcs on localhost:${PORT}"
+echo "suggested urls:"
+echo "Scott's arc-stories: http://localhost:${PORT}/arcs-cdn/dev/apps/web/?manifest=http://localhost:${PORT}/arc-stories/0.2/arc.manifest"
+echo "Matt's arcs-particles: http://localhost:${PORT}/arcs-cdn/dev/apps/web/?manifest=http://localhost:${PORT}/arcs-particles/arcs-particles.manifest"
+./server.py -p ${PORT}
